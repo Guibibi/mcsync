eaa696c60637810595fe24c0551395b2d60362c7
Guillaume Bedard <guillaume.bedard@planethoster.info>
2021-03-06 17:20:16 -0500

    Add files check

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
-var fs = require('fs')
-var net = require('net')
-var chalk = require('chalk')
-var path = require('path')
+import chalk from 'chalk'
+import * as fs from 'fs'
+import * as net from 'net'
+import * as path from 'path'
 
 const port = 3456
 const isRunning = false
@@ -10,11 +10,15 @@ const serverFiles: any[] = []
 export function main() {
   console.log('hello server')
   fs.readdir('./mods', (err: any, files: any[]) => {
+    if (err) return console.log(chalk.red('No mods folder found!'))
     files.forEach((file: any) => {
       serverFiles.push(file)
     })
     console.log(serverFiles)
     if ((serverFiles.length = 0)) {
+      return false
+    } else {
+      console.log(chalk.green(serverFiles.toString()))
     }
   })
 }
